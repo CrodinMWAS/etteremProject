b2a1e25f8fb45ad709e2babd888dabb94a1e848e
KunAttila69 <klun.attila0508@gmail.com>
2024-09-18 18:31:52 +0200

    registration frontend finished

--- Frontend/Frontend/Frontend/Registration.xaml.cs
+++ Frontend/Frontend/Frontend/Registration.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,23 +26,96 @@ namespace Frontend
             InitializeComponent();
         }
 
-        private void Login(object sender, RoutedEventArgs e)
+        private void LoadLogin()
         {
             this.Close();
             Login login = new Login();
-            login.Show(); 
+            login.Show();
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            LoadLogin();
+        }
+
+        private void ResetFields()
+        { 
+            tb_name.BorderBrush = Brushes.Black;
+            err_name.Content = "";
+            tb_email.BorderBrush = Brushes.Black;
+            err_email.Content = "";
+            pb_password.BorderBrush = Brushes.Black;
+            err_password.Content = "";
+            pb_confirmPassword.BorderBrush = Brushes.Black;
+            err_confirmPassword.Content = "";
         }
 
         private void btn_login_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_name.Text == "" || tb_email.Text == "" || pb_password.Password == "" || pb_confirmPassword.Password == "")
+            ResetFields();
+            int[] errors = { 0, 0, 0, 0 };
+            if (tb_name.Text == "")
+            { 
+                errors[0] = 1;
+            }
+            if (tb_email.Text == "")
+            { 
+                errors[1] = 1;
+            }
+            if (!Regex.IsMatch(tb_email.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
             {
-                MessageBox.Show("Ki kell tölteni az összes mezőt");
-                return;
-            }else if (pb_confirmPassword != pb_password)
+                errors[1] = 2; 
+            }
+            if (pb_password.Password == "")
+            { 
+                errors[2] = 1;
+            }
+            if (pb_confirmPassword.Password == "")
+            { 
+                errors[3] = 1;
+            }
+            if (pb_confirmPassword.Password != pb_password.Password)
             {
-                MessageBox.Show("A jelszó és annak megerősítése nem egyeznek");
-                return;
+                errors[3] = 2;
+            }
+
+            int errorCount = 0;
+            for (int i = 0; i < errors.Length; i++)
+            {
+                if (errors[i] > 0)
+                {
+                    errorCount++;
+                    switch (i)
+                    {
+                        case 0:
+                            tb_name.BorderBrush = Brushes.Red;
+                            err_name.Content = "Empty field";
+                            break;
+                        case 1:
+                            tb_email.BorderBrush = Brushes.Red;
+                            err_email.Content = errors[i] == 1 ? "Empty field" : "Not valid format";
+                            break;
+                        case 2:
+                            pb_password.BorderBrush = Brushes.Red;
+                            err_password.Content = "Empty field";
+                            break;
+                        case 3:
+                            pb_confirmPassword.BorderBrush = Brushes.Red;
+                            err_confirmPassword.Content = errors[i] == 1 ? "Empty field" : "Passwords don't match";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            if (errorCount == 0)
+            {
+                LoadLogin();
+            }
+            else
+            {
+                MessageBox.Show("Error");
             }
         }
     }
