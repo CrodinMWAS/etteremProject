36a085fa51028f70a03f15624b3cc4992233fa53
KunAttila69 <klun.attila0508@gmail.com>
2024-09-22 22:25:31 +0200

    fixes

--- Frontend/Frontend/Frontend/MainWindow.xaml.cs
+++ Frontend/Frontend/Frontend/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Frontend
         private void btn_login_Click(object sender, RoutedEventArgs e)
         {
             Login login = new Login();
-            login.Show();
+            login.ShowDialog();
         }
 
         private void ResponsiveText(object sender, SizeChangedEventArgs e)
@@ -36,12 +36,17 @@ namespace Frontend
             double newFontSize = Math.Min(e.NewSize.Width / 20, e.NewSize.Height / 10)/40; 
             txt1.FontSize = 40 * newFontSize;
             txt2.FontSize = 20 * newFontSize;
-            txt3.FontSize = 12 * newFontSize;
+            txt3.FontSize = 14 * newFontSize;
             txt4.FontSize = 15 * newFontSize;
-            btn_login.FontSize = 15 * newFontSize;
-            lbl_Navbar.FontSize = 20 * newFontSize;
+            btnLogin.FontSize = 15 * newFontSize;
+            lblNavbar.FontSize = 20 * newFontSize;
             rotateTransform.CenterX = 125 * newFontSize;
             rotateTransform.CenterY = 125 * newFontSize;
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
